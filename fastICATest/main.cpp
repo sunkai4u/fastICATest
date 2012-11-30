7b591ee2d4f5f1ad6c7ad8bc43b7e79b58f4e3bc
sunkai <sunkai@sunkai.local>
2012-11-30 18:28:43 +0800

    Initial Commit

---
+++ fastICATest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fastICATest
+//
+//  Created by sunkai on 12-11-30.
+//  Copyright (c) 2012年 VisGenius. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
